863a556f76465cec81e36a6561fdffb581044bfb
Islamomer47 <islamomar0003@gmail.com>
2024-10-05 22:20:10 +0300

    fixed the connection fo DB

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,27 @@
+require("dotenv").config();
 const { Pool } = require("pg");
+
+// Function to encode the password
+const encodePassword = (password) => {
+  return encodeURIComponent(password);
+};
+
+const connectionString = `postgresql://${process.env.DB_USER}:${encodePassword(
+  process.env.DB_PASSWORD
+)}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: connectionString,
 });
+
+// Log connection status
+pool
+  .connect()
+  .then(() => {
+    console.log("Connected to the PostgreSQL database successfully!");
+  })
+  .catch((err) => {
+    console.error("Connection to the PostgreSQL database failed:", err.stack);
+  });
 
 module.exports = pool;
